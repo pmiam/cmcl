a57131a9218da05d9f59dfe885738f7712e6d725
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-02-07 21:53:32 -0500

    started file for catagorizers based on various feature tables

---
+++ cmcl/features/extract_categories.py
@@ -0,0 +1,43 @@
+
+
+def categorize_mix(df):
+   """
+   Take a perovskites dataframe's composition matrix, and generate a new Mixing column
+   """
+   A_cols = []
+   B_cols = []
+   X_cols = []
+   for label in perovskite_site_members.values:
+       if label[0] and label[0] in df:
+           A_cols.append(label[0])
+       if label[1] and label[1] in df:          
+           B_cols.append(label[1])
+       if label[2] and label[2] in df:
+           X_cols.append(label[2])
+   A_site_occup = df[A_cols]
+   B_site_occup = df[B_cols]
+   X_site_occup = df[X_cols]
+   #if status is not exactly 1 in each row, append to mixtring and set row Mixing to mixstring
+   A_site_stat = A_site_occup.notna().sum(axis=1)
+   B_site_stat = B_site_occup.notna().sum(axis=1)
+   X_site_stat = X_site_occup.notna().sum(axis=1)
+   mixlog = pd.concat([A_site_stat, B_site_stat, X_site_stat], axis=1)
+
+   def mixreader(row):
+      mixstring = " & "
+      stringlist=[]
+      if row[0] != 1:
+         stringlist.append("A")
+      if row[1] != 1:
+         stringlist.append("B")
+      if row[2] != 1:
+         stringlist.append("X")
+      if stringlist:
+         stringlist[-1] = stringlist[-1] + "-site"
+      if not stringlist:
+         stringlist.append("Pure")
+      mixstring = mixstring.join(stringlist)
+      return mixstring
+
+   df.Mixing = mixlog.apply(lambda row: mixreader(row), axis=1)
+     
--- cmcl/features/extract_constituents.py
+++ cmcl/features/extract_constituents.py
@@ -193,7 +193,8 @@ class CompositionTable():
         comp_cols_idx = is_not_original_content(updated)
         logging.debug(f"bool cols: {comp_cols_idx}")
         comp_cols = updated[comp_cols_idx]
-        logging.debug(f"comp cols: {comp_cols}")        
+        logging.debug(f"comp cols: {comp_cols}")
+        logging.debug(f"comp cols type: {type(comp_cols)}")
         return comp_cols
 
     def make_and_get(self):
@@ -201,47 +202,3 @@ class CompositionTable():
         self.df = self.df.assign(**comp_dict)
         comp_cols = self.get()
         return comp_cols
-
-def catagorize_mix(df):
-   """
-   Take a perovskites dataframe including a composition matrix, and generate a new Mixing column
-
-   This mutates the passed dataframe in place
-   """
-   A_cols = []
-   B_cols = []
-   X_cols = []
-   for label in perovskite_site_members.values:
-       if label[0] and label[0] in df:
-           A_cols.append(label[0])
-       if label[1] and label[1] in df:          
-           B_cols.append(label[1])
-       if label[2] and label[2] in df:
-           X_cols.append(label[2])
-   A_site_occup = df[A_cols]
-   B_site_occup = df[B_cols]
-   X_site_occup = df[X_cols]
-   #if status is not exactly 1 in each row, append to mixtring and set row Mixing to mixstring
-   A_site_stat = A_site_occup.notna().sum(axis=1)
-   B_site_stat = B_site_occup.notna().sum(axis=1)
-   X_site_stat = X_site_occup.notna().sum(axis=1)
-   mixlog = pd.concat([A_site_stat, B_site_stat, X_site_stat], axis=1)
-
-   def mixreader(row):
-      mixstring = " & "
-      stringlist=[]
-      if row[0] != 1:
-         stringlist.append("A")
-      if row[1] != 1:
-         stringlist.append("B")
-      if row[2] != 1:
-         stringlist.append("X")
-      if stringlist:
-         stringlist[-1] = stringlist[-1] + "-site"
-      if not stringlist:
-         stringlist.append("Pure")
-      mixstring = mixstring.join(stringlist)
-      return mixstring
-
-   df.Mixing = mixlog.apply(lambda row: mixreader(row), axis=1)
-     
