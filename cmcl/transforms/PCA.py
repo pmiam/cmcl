8b29979c85c7316acef77c093b899788658eedb3
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-02-10 01:05:37 -0500

    starting transformers api

---
+++ cmcl/transforms/PCA.py
@@ -0,0 +1,7 @@
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+class PCA():
+    """dataframe accessor PCA transformer"""
+    def __init__(self):
+        pass
