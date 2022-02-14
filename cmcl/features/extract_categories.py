03b98adb080d65b40a8023e2a5a819119e20134a
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-02-14 13:12:55 -0500

    updated docstring. aligned with new FeatureAccessor design

--- cmcl/features/extract_categories.py
+++ cmcl/features/extract_categories.py
@@ -5,7 +5,7 @@ class DummyTable():
     def __init__(self, df):
         self._validate(df)
         self.df = df
-        self._cols_before_update = df.columns.values
+        self._original = df.columns.values
 
     @staticmethod
     def _validate(df):
@@ -13,6 +13,13 @@ class DummyTable():
         id cols with lists inside or dicts inside
         warn
         """
+        def conditional_casefold(entry):
+            if isinstance(entry, str):
+                return entry.casefold()
+            else:
+                return entry
+        validx = ["Formula".casefold() in ref for ref in map(conditional_casefold, self.df.columns)]
+
         for nncol_name in df.select_dtypes(include=["object"]).columns:
             el_not_str = df[nncol_name].apply(lambda x: not isinstance(x, str))
             log = f"""These records contain non-numeric, non-string dtypes:
@@ -20,25 +27,24 @@ class DummyTable():
             if el_not_str.any():
                 logging.critical(log)
                 raise ValueError("data is not suitable for one-hot encoding. see log")
-            
 
-    def make_and_get(self):
+    def make(self):
         """
-        one-hot-encode categorical variables. Not including Formula, so it can be used on base input easily.
-        can't imagine why you'd want to ohe formula strings
+        one-hot-encode categorical variables in given df. (Not
+        including Formula column).
+        
+        Convenience for using catagorical variables in models.
 
         wishlist: develop logic for prefixing dummy cols
         """
         #make in one swoop
-        self.df = pd.get_dummies(self.df.loc[:, self.df.columns != "Formula"])
+        self.df = pd.get_dummies(self.df.loc[:, validx])
         #get indices
-        original = self._cols_before_update
         updated = self.df.columns.values
-        is_not_original_content = np.vectorize(lambda x: x not in original)
+        is_not_original_content = np.vectorize(lambda x: x not in self._original)
         ohe_cols_idx = is_not_original_content(updated)
         ohe_cols = updated[ohe_cols_idx]
-        return ohe_cols
-
+        return self.df[ohe_cols]
 
 class LabelGrouper():
    """
