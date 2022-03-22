201270e1c7545c9eb565c88cfe2d1e26e3020964
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-03-22 14:25:33 -0400

    cmcl dbs live here now. Starting to build access infrastructure

---
+++ cmcl/db/access.py
@@ -0,0 +1,22 @@
+"""this file entirely copied from Immentel's Mendeleev library"""
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+perovsdb = "perovskites.db"
+
+def get_cmcl_perovs_path():
+    """Return the Mannodi Group Perovskites database path"""
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), perovsdb)
+
+def get_engine(dbpath=None):
+    """Return the db engine -- could be made to take a switch for choosing datasets"""
+    if not dbpath:
+        dbpath = get_cmcl_perovs_path()
+    return create_engine(f"sqlite:///{str(dbpath)}", echo=False)
+
+def get_session(dbpath=None):
+    """Return the database session connection."""
+    engine = get_engine(dbpath=dbpath)
+    db_session = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+    return db_session()
