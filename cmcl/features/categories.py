36799654a7bd3031255d708b20d68e510c2885e8
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-03-25 14:43:35 -0400

    libraries re-arranged

--- cmcl/features/categories.py
+++ cmcl/features/categories.py
@@ -5,6 +5,38 @@ logging.basicConfig(level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S", format=logf
 import pandas as pd
 import numpy as np
 
+class Categories():
+    """
+    Tools for simplifying the generation of categorical columns based
+    on the values of data frames.
+    
+    Ideal for producing scatter plot labels.
+    """
+    @staticmethod
+    def logif(df, condition, default=None):
+        """
+        produce a series of categorical strings based on the numerical
+        contents of a dataframe, its column names, and some condition
+        described as a function returning True. Optionally provide a
+        default label for records not meeting condition in any column.
+
+        example:
+        df.pipe(Categories.logif, condition=lambda x: x>1, default="pure")
+        """
+        def _logif(row):
+            catstring = " & "
+            stringlist=[]
+            for entry, label in zip(row, df.columns):
+                if condition(entry):
+                    stringlist.append(label)
+            if not stringlist:
+                stringlist.append(str(default))
+            catstring = catstring.join(stringlist)
+            return catstring
+        catseries = df.apply(_logif, axis=1)
+        return catseries
+
+#to be replaced with sklearn compatibility transformer
 class DummyTable():
     def __init__(self, df):
         self._validate(df)
