a8fd4dd221b30f5dd84056c621d4bf951e919e11
Panayotis Manganaris <panos.manganaris@gmail.com>
2022-03-26 15:36:40 -0400

    removed ft.ohe. moved collect accessor. updated model doc.

--- cmcl/data/frame.py
+++ cmcl/data/frame.py
@@ -4,18 +4,15 @@ logging.basicConfig(level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S", format=logf
 
 import pandas as pd
 import numpy as np
-#access feature computers
-from cmcl.features.extract_constituents import CompositionTable
+#metadata handling
+from cmcl.data.index import ColumnGrouper, ColumnHandler
 
-#access transformers
-from cmcl.features.extract_categories import DummyTable
-from cmcl.transforms.PCA import PCA
+#feature computers
+from cmcl.features.extract_constituents import CompositionTable
 
 #access models
 from cmcl.models.RFR import RFR
 
-#metadata handling
-from cmcl.data.index import ColumnGrouper
 
 # feature accessors should saveable in some way for future use/reporting
 
@@ -59,12 +56,6 @@ class FeatureAccessor():
     def base(self):
         return self._df
         
-    def ohe(self, regen=False):
-        if self._ohedf is None or regen:
-            feature = DummyTable(self._df)
-            self._ohedf = feature.make()
-        return self._ohedf
-
     def comp(self, regen=False):
         """
         Default call accesses or creates composition table.
@@ -91,6 +82,112 @@ class FeatureAccessor():
     def meg(self):
         """as above, get array of MEGnet elemental embeddings"""
         print("megnet Not Implemented")
+
+@pd.api.extensions.register_dataframe_accessor("tf")
+class TransformAccessor():
+    """
+    Define and retrieve transforms of tables. When transform does not
+    exist, it will be created.
+    
+    Supports generic Scikit-Learn contextual transforms using the
+    df.tf.pipe() method. This simply inserts a thin wrapper between
+    the df.pipe() method and functions that act on Dataframes,
+    returning multidimensional arrays (like Scikit-Learn
+    Transformers).
+
+    example:
+    X = X.tf.pipe(StandardScaler().fit_transform)
+    Xpca = X.tf.pca()
+
+    In this way, transforms stay fully contextualized by the dataframe
+    indices
+    """
+    def __init__(self, df):
+        self._validate(df)
+        #getting original
+        self._df = df
+        #getting mix categories
+        self._PCA_mod_df = None
+        self._PCA_cols = None
+        
+    @staticmethod
+    def _validate(df):
+        """
+        verify df contains numerical data of the float dtype
+        """
+        pass
+
+    def base(self):
+        return self._df
+        
+    def _make_pca(self):
+        extender = PCATable(self._df)
+        self._pca_cols = extender.make_and_get()
+        self._pca_mod_df = extender.df
+
+    def _get_pca(self):
+        return self._pca_mod_df[self._pca_cols]
+        
+    def pca(self):
+        #wishlist: make the score matrix recoverable for biplotting
+        if self._pca_cols is None:
+            self._make_pca()
+        return self._get_pca()
+
+@pd.api.extensions.register_dataframe_accessor("model")
+class ModelAccessor():
+    """
+    Generate models of tables based on other tables
+    
+    automates the construction of a sklearn compliant pipeline. This includes:
+    - pandas index manipulation such that results are appropriately labeled
+    - 0.8train/0.2test split (default if not specified by the user)
+    - labeling records with their respective partition
+    - application of estimator
+    
+    An accessed table will have models created for it. models are
+    stored with the accessor instance, predictions are not.
+
+    Y.model.pipe(RandomForestRegressor.fit(X, Y).predict(X)))
+    """
+    def __init__(self, Y):
+        self._validate(Y)
+        #getting original
+        self._df = Y
+        #existing models
+        self._RFR = None
+        
+    @staticmethod
+    def _validate(Y):
+        """
+        verify Y contains numerical data
+
+        flag weather categorical or continuous?
+        """
+        pass
+    
+    def base(self):
+        return self._df
+    
+    def _do_RFR(self, X, **kwargs): #extend args?
+        modeler = RFR(X, self._df, **kwargs)
+        modeler.train_test_return()
+        self._RFR = modeler
+    
+    def RFR(self, X, **kwargs):
+        """
+        return a model of Y based on X, The form of X used,
+        and optionally the model used to get Y.
+
+        Both Y and X are returned with pandas multindex
+
+        this can be used to access the train/test split for each
+        dataframe conveniently using pandas tuple indexing, the pandas
+        IndexSlice module, or the .xs (cross section) method.
+        """
+        if self._RFR is None:
+            self._do_RFR(X, **kwargs)
+        return self._RFR.Y_stack, self._RFR.X_stack, self._RFR.r
 
 @pd.api.extensions.register_dataframe_accessor("collect")
 class CollectionAccessor():
@@ -149,110 +246,3 @@ class CollectionAccessor():
                     "B":["Pb", "Sn", "Ge", "Ba", "Sr", "Ca", "Be", "Mg", "Si", "V", "Cr", "Mn", "Fe", "Ni", "Zn", "Pd", "Cd", "Hg"],
                     "X":["I", "Br", "Cl"]}
         return self.by(segments, ("site","element"))
-
-@pd.api.extensions.register_dataframe_accessor("tf")
-class TransformAccessor():
-    """
-    Define and retrieve transforms of tables. When transform does not
-    exist, it will be created.
-    
-    Supports generic Scikit-Learn contextual transforms using the
-    df.tf.pipe() method. This simply inserts a thin wrapper between
-    the df.pipe() method and functions that act on Dataframes,
-    returning multidimensional arrays (like Scikit-Learn
-    Transformers).
-
-    example:
-    X = X.tf.pipe(StandardScaler().fit_transform)
-    Xpca = X.tf.pca()
-
-    In this way, transforms stay fully contextualized by the dataframe
-    indices
-    """
-    def __init__(self, df):
-        self._validate(df)
-        #getting original
-        self._df = df
-        #getting mix categories
-        self._PCA_mod_df = None
-        self._PCA_cols = None
-        
-    @staticmethod
-    def _validate(df):
-        """
-        verify df contains numerical data of the float dtype
-        """
-        pass
-
-    def base(self):
-        return self._df
-        
-    def _make_pca(self):
-        extender = PCATable(self._df)
-        self._pca_cols = extender.make_and_get()
-        self._pca_mod_df = extender.df
-
-    def _get_pca(self):
-        return self._pca_mod_df[self._pca_cols]
-        
-    def pca(self):
-        #wishlist: make the score matrix recoverable for biplotting
-        if self._pca_cols is None:
-            self._make_pca()
-        return self._get_pca()
-
-@pd.api.extensions.register_dataframe_accessor("model")
-class ModelAccessor():
-    """
-    Conveniently and robustly define and retrieve models of tables based on other tables
-
-    An accessed table will have models created for it. models are
-    stored with the accessor instance, predictions are not. 
-
-    predictions and models can be obtained with a simple one-liner
-    
-    Y.model.RFR(X, optimize=True)
-
-    provides
-    RFR
-    GRR
-    NN
-    """
-    def __init__(self, Y):
-        self._validate(Y)
-        #getting original
-        self._df = Y
-        #existing models
-        self._RFR = None
-        
-    @staticmethod
-    def _validate(Y):
-        """
-        verify Y contains numerical data
-
-        flag weather categorical or continuous?
-        """
-        pass
-    
-    def base(self):
-        return self._df
-    
-    def _do_RFR(self, X, **kwargs): #extend args?
-        modeler = RFR(X, self._df, **kwargs)
-        modeler.train_test_return()
-        self._RFR = modeler
-    
-    def RFR(self, X, **kwargs):
-        """
-        return a model of Y based on X, The form of X used,
-        and optionally the model used to get Y.
-
-        Both Y and X are returned with pandas multindex
-
-        this can be used to access the train/test split for each
-        dataframe conveniently using pandas tuple indexing, the pandas
-        IndexSlice module, or the .xs (cross section) method.
-        """
-        if self._RFR is None:
-            self._do_RFR(X, **kwargs)
-        return self._RFR.Y_stack, self._RFR.X_stack, self._RFR.r
